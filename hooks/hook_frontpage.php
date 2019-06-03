b2af4cf3b8aef5da2123f191ab0c4f4f5103f6d7
Tim van Dijen <tim.dijen@minbzk.nl>
2019-06-03 10:46:29 +0200

    Revert BC-breaking Webmozart-assertions

--- hooks/hook_frontpage.php
+++ hooks/hook_frontpage.php
@@ -1,7 +1,5 @@
 <?php
 
-use Webmozart\Assert\Assert;
-
 /**
  * Hook to add the modinfo module to the frontpage.
  *
@@ -10,8 +8,8 @@ use Webmozart\Assert\Assert;
  */
 function statistics_hook_frontpage(&$links)
 {
-    Assert::isArray($links);
-    Assert::keyExists($links, 'links');
+    assert(is_array($links));
+    assert(array_key_exists('links', $links));
 
     $links['config']['statistics'] = [
         'href' => SimpleSAML\Module::getModuleURL('statistics/showstats.php'),
